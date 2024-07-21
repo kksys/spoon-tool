0cba4b29260550f57805ec96d534c15badc1db3c
kksys <78661652+kksys@users.noreply.github.com>
2024-07-21 18:39:43 +0000

    add implementation

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,18 +1,59 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Context, Hono } from "hono";
+import { cors } from "hono/cors";
 
-export default {
-	async fetch(_request, _env, _ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+const app = new Hono()
+
+app.use(cors({
+  origin: ['https://spoon-tool.kk-systems.net/'],
+  allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'],
+  allowMethods: ['GET', 'OPTIONS'],
+  exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+  maxAge: 600,
+  credentials: true,
+}))
+
+const isRequestFromFrontend = (c: Context) =>
+  (
+    !!c.req.header('host')?.match(/api\.spoon-tool\.kk-systems\.net/) &&
+    !!c.req.header('origin')?.match(/spoon-tool\.kk-systems\.net/)
+  )
+
+app.use(async (c, next) => {
+  if (!isRequestFromFrontend(c)) {
+    return c.json({
+      error: 'Forbidden'
+    }, 403)
+  }
+
+  await next()
+})
+
+app.get('/search/user/', async (c) => {
+  const query = new URLSearchParams(c.req.query())
+  const searchUserUrl = `https://jp-api.spooncast.net/search/user/?${query}`
+
+  const response = await fetch(searchUserUrl, {
+    method: 'GET'
+  })
+
+  return new Response(await response.arrayBuffer(), response)
+})
+
+app.get('/users/:userId', async (c) => {
+  const { userId } = c.req.param()
+  const getProfileUrl = `https://jp-api.spooncast.net/users/${userId}`
+
+  const response = await fetch(getProfileUrl, {
+    method: 'GET'
+  })
+
+  return new Response(await response.arrayBuffer(), response)
+})
+
+app.all('*', (c) => {
+  return c.json({
+    error: 'Not Found'
+  }, 404)
+})
+
+export default app

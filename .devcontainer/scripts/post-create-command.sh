6f4ba8e073e8532c16157ed4e4b85ab45bb83b5e
kksys <78661652+kksys@users.noreply.github.com>
2024-08-24 18:46:37 +0000

    upgrade pnpm to v9.8.0

--- .devcontainer/scripts/post-create-command.sh
+++ .devcontainer/scripts/post-create-command.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
+NODE_VERSION=$(cat package.json | jq -r .engines.node)
+PNPM_VERSION=$(cat package.json | jq -r .engines.pnpm)
+
+echo "node version: ${NODE_VERSION}"
+echo "pnpm version: ${PNPM_VERSION}"
+echo "home: $HOME"
+
+# install and activate specified node version
+nvm install ${NODE_VERSION}
+nvm use ${NODE_VERSION}
+
+# install and activate specified pnpm version
+pnpm setup
+source $HOME/.bashrc
+pnpm i -g pnpm@${PNPM_VERSION}
+source $HOME/.bashrc
+
 pnpm i
 ./build-scripts/generate-env.sh

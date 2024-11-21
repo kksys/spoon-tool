345fcd41f453af5b97e22b07587002ac51bb11b9
kksys <kk.systems.develop@gmail.com>
2024-11-22 03:25:32 +0900

    modified script to support other than codespace environment

--- .devcontainer/scripts/post-attach-command.sh
+++ .devcontainer/scripts/post-attach-command.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-${CODESPACE_VSCODE_FOLDER}/.devcontainer/scripts/backend-port-service
+# launch the backend port service (this is only needed in codespaces)
+if [ "$CODESPACE" == 'true' ]; then
+  ${CODESPACE_VSCODE_FOLDER}/.devcontainer/scripts/backend-port-service
+else
+  echo "Not in a codespace, skipping backend port service setup"
+fi

c841684677840f51208ca912f74a39fefbc16d50
kksys <kk.systems.develop@gmail.com>
2024-11-24 01:27:48 +0900

    delete no longer use features

--- tools/devcontainer/.devcontainer/local-features/setup-user/install.sh
+++ tools/devcontainer/.devcontainer/local-features/setup-user/install.sh
@@ -25,55 +25,70 @@ sudo_if() {
   fi
 }
 
-# Enables the oryx tool to generate manifest-dir which is needed for running the postcreate tool
-DEBIAN_FLAVOR="focal-scm"
-mkdir -p /opt/oryx && echo "vso-focal" > /opt/oryx/.imagetype
-echo "DEBIAN|${DEBIAN_FLAVOR}" | tr '[a-z]' '[A-Z]' > /opt/oryx/.ostype
+if [ -e /usr/local/oryx ]; then
+  # Enables the oryx tool to generate manifest-dir which is needed for running the postcreate tool
+  DEBIAN_FLAVOR="focal-scm"
+  mkdir -p /opt/oryx && echo "vso-focal" > /opt/oryx/.imagetype
+  echo "DEBIAN|${DEBIAN_FLAVOR}" | tr '[a-z]' '[A-Z]' > /opt/oryx/.ostype
 
-# Oryx expects the tool to be installed at `/opt/oryx` and looks for relevant files in there.
-ln -snf /usr/local/oryx/* /opt/oryx
+  # Oryx expects the tool to be installed at `/opt/oryx` and looks for relevant files in there.
+  ln -snf /usr/local/oryx/* /opt/oryx
+fi
 
 # For the universal image, oryx build tool installs the detected platforms in /home/codespace/*. Hence, linking current platforms to the /home/codespace/ path and adding it to the PATH.
 # This ensures that whatever platfornm versions oryx detects and installs are set as root.
 NODE_PATH="/home/codespace/nvm/current"
-ln -snf /usr/local/share/nvm /home/codespace
+if [ -e /usr/local/share/nvm ]; then
+  ln -snf /usr/local/share/nvm /home/codespace
+fi
 
 PHP_PATH="/home/${USERNAME}/.php/current"
-mkdir -p /home/${USERNAME}/.php
-ln -snf /usr/local/php/current $PHP_PATH
+if [ -e /usr/local/php/current ]; then
+  mkdir -p /home/${USERNAME}/.php
+  ln -snf /usr/local/php/current $PHP_PATH
+fi
 
 PYTHON_PATH="/home/${USERNAME}/.python/current"
-mkdir -p /home/${USERNAME}/.python
-ln -snf /usr/local/python/current $PYTHON_PATH
-ln -snf /usr/local/python /opt/python
+if [ -e /usr/local/python/current ]; then
+  mkdir -p /home/${USERNAME}/.python
+  ln -snf /usr/local/python/current $PYTHON_PATH
+  ln -snf /usr/local/python /opt/python
+fi
 
 JAVA_PATH="/home/codespace/java/current"
-ln -snf /usr/local/sdkman/candidates/java /home/codespace
+if [ -e /usr/local/sdkman/candidates/java ]; then
+  ln -snf /usr/local/sdkman/candidates/java /home/codespace
+fi
 
 RUBY_PATH="/home/${USERNAME}/.ruby/current"
-mkdir -p /home/${USERNAME}/.ruby
-ln -snf /usr/local/rvm/rubies/default $RUBY_PATH
+if [ -e /usr/local/rvm/rubies/default ]; then
+  mkdir -p /home/${USERNAME}/.ruby
+  ln -snf /usr/local/rvm/rubies/default $RUBY_PATH
+fi
 
 DOTNET_PATH="/home/${USERNAME}/.dotnet"
+if [ -e /usr/share/dotnet ]; then
+  # Required due to https://github.com/devcontainers/features/pull/628/files#r1276659825
+  chown -R "${USERNAME}:${USERNAME}" /usr/share/dotnet
+  chmod g+r+w+s /usr/share/dotnet
+  chmod -R g+r+w /usr/share/dotnet
 
-# Required due to https://github.com/devcontainers/features/pull/628/files#r1276659825
-chown -R "${USERNAME}:${USERNAME}" /usr/share/dotnet
-chmod g+r+w+s /usr/share/dotnet
-chmod -R g+r+w /usr/share/dotnet
-
-ln -snf /usr/share/dotnet $DOTNET_PATH
-mkdir -p /opt/dotnet/lts
-cp -R /usr/share/dotnet/dotnet /opt/dotnet/lts
-cp -R /usr/share/dotnet/LICENSE.txt /opt/dotnet/lts
-cp -R /usr/share/dotnet/ThirdPartyNotices.txt /opt/dotnet/lts
+  ln -snf /usr/share/dotnet $DOTNET_PATH
+  mkdir -p /opt/dotnet/lts
+  cp -R /usr/share/dotnet/dotnet /opt/dotnet/lts
+  cp -R /usr/share/dotnet/LICENSE.txt /opt/dotnet/lts
+  cp -R /usr/share/dotnet/ThirdPartyNotices.txt /opt/dotnet/lts
+fi
 
 MAVEN_PATH="/home/${USERNAME}/.maven/current"
 mkdir -p /home/${USERNAME}/.maven
 ln -snf /usr/local/sdkman/candidates/maven/current $MAVEN_PATH
 
 HUGO_ROOT="/home/${USERNAME}/.hugo/current"
-mkdir -p /home/${USERNAME}/.hugo
-ln -snf /usr/local/hugo $HUGO_ROOT
+if [ -e /usr/local/hugo ]; then
+  mkdir -p /home/${USERNAME}/.hugo
+  ln -snf /usr/local/hugo $HUGO_ROOT
+fi
 
 HOME_DIR="/home/${USERNAME}/"
 chown -R ${USERNAME}:${USERNAME} ${HOME_DIR}
@@ -81,11 +96,37 @@ chmod -R g+r+w "${HOME_DIR}"
 find "${HOME_DIR}" -type d | xargs -n 1 chmod g+s
 
 OPT_DIR="/opt/"
-chown -R ${USERNAME}:oryx ${OPT_DIR}
-chmod -R g+r+w "${OPT_DIR}"
-find "${OPT_DIR}" -type d | xargs -n 1 chmod g+s
+if [ $(getent group oryx) ]; then
+  chown -R ${USERNAME}:oryx ${OPT_DIR}
+  chmod -R g+r+w "${OPT_DIR}"
+  find "${OPT_DIR}" -type d | xargs -n 1 chmod g+s
+fi
 
-echo "Defaults secure_path=\"${DOTNET_PATH}:${NODE_PATH}/bin:${PHP_PATH}/bin:${PYTHON_PATH}/bin:${JAVA_PATH}/bin:${RUBY_PATH}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:/usr/local/share:/home/${USERNAME}/.local/bin:${PATH}\"" >> /etc/sudoers.d/$USERNAME
+append_secure_path() {
+  SECURE_PATH=""
+  if [ -e "${DOTNET_PATH}" ]; then
+    SECURE_PATH+="${DOTNET_PATH}:"
+  fi
+  if [ -e "${NODE_PATH}/bin" ]; then
+    SECURE_PATH+="${NODE_PATH}/bin:"
+  fi
+  if [ -e "${PHP_PATH}/bin" ]; then
+    SECURE_PATH+="${PHP_PATH}/bin:"
+  fi
+  if [ -e "${PYTHON_PATH}/bin" ]; then
+    SECURE_PATH+="${PYTHON_PATH}/bin:"
+  fi
+  if [ -e "${JAVA_PATH}/bin" ]; then
+    SECURE_PATH+="${JAVA_PATH}/bin:"
+  fi
+  if [ -e "${RUBY_PATH}/binn" ]; then
+    SECURE_PATH+="${RUBY_PATH}/bin:"
+  fi
+  SECURE_PATH+="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:/usr/local/share:/home/${USERNAME}/.local/bin:${PATH}"
+
+  echo "Defaults secure_path=\"${SECURE_PATH}\"" >> /etc/sudoers.d/$USERNAME
+}
+append_secure_path
 
 # Temporary: Due to GHSA-c2qf-rxjj-qqgw
 bash -c ". /usr/local/share/nvm/nvm.sh && nvm use 18"
